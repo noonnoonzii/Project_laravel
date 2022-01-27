d0e34c3034f9e5b96d587d1fc4759350464aaca5
noonnoonzii <noonlovechelsea_1905@hotmail.com>
2022-01-27 11:08:57 +0700

    API Test
    Nattakarn Work

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/product',[\App\Http\Controllers\API\ProductController::class,'index']);
+Route::get('/category',[\App\Http\Controllers\API\CategoryController::class,'index']);
+Route::get('/news',[\App\Http\Controllers\API\NewsController::class,'index']);
+Route::get('/users',[\App\Http\Controllers\API\UsersController::class,'index']);
